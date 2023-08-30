b6c5fe65bcc3c214982c56c7687c366d21adcbf1
natnaelfantaw <yimerfnat@gmail.com>
2023-08-30 01:37:08 -0700

    al

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -28,13 +28,3 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             new_list.append(div)
     return (new_list)
-
-=============================
-5-raise_exception.py
-
-#!/usr/bin/python3
-
-
-def raise_exception():
-    """Raise a TypeError exception."""
-    raise TypeError
